606beb42ac6b7fa3d49430a5a5be1d61d4e8d8d3
Alavalapalli Vasanth Reddy <alavalapalligithub@gmail.com>
2023-10-10 17:29:16 +0530

    updated python code

--- python code.py
+++ python code.py
@@ -1,8 +1,7 @@
-
-    
-
 num = int(input("Enter any number to test whether it is odd or even:"))
 if(num % 2) == 0:
     print("The number is even")
 else:
     print('The provided number is odd')
+    
+#it is odd
